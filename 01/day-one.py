f8a66e749d7d390a320796725d46d199d8f1151d
antooni <antoni.pawlak@protonmail.com>
2021-12-01 10:55:02 +0100

    feat(day-01): submarine

---
+++ 01/day-one.py
@@ -0,0 +1,43 @@
+# PART 1
+counter = 0
+previous = -1
+
+with open('input.txt') as f:
+    for line in f:
+        if previous == -1:
+            previous = int(line)
+            continue
+        if previous < int(line):
+            counter += 1
+        previous = int(line)
+        
+print('Part 1 solution: ',counter)
+
+#PART 2
+counter = 0
+lines = []
+
+with open('input.txt') as f:
+    line = f.readline()
+    while line:
+        lines.append(int(line))
+        line = f.readline()
+
+n = len(lines)
+
+for i, line in enumerate(lines):
+    if (i+3) >= n:
+        break
+
+    a = lines[i] + lines[i+1] + lines[i+2]
+    b = lines[i+1] + lines[i+2] + lines[i+3]
+
+    if a < b:
+        counter += 1
+
+print('Part 2 solution: ',counter)
+    
+
+
+
+
